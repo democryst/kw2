b208e32701654c55951717b8e068570ef20b5195
Pawit Manussirivitaya <democryst@gmail.com>
2017-01-31 11:18:34 +0700

    edit wfdoc 11:18 31-1-60

--- createformType_geninfo_handle.php
+++ createformType_geninfo_handle.php
@@ -12,24 +12,24 @@ $sys_adminid = 3;
 $date_hrs = date("h-i-s");
 $date_day = date("Y-m-d");
 $all_date = $date_hrs . '***' . $date_day;
-/*
+
 //upload to wfgeninfo
 			// require user and user group to exist in DB
 				$q_INSERT_wfgeninfo="INSERT INTO `wfgeninfo`(`CreateTime`, `FormName`, `Description`, `AdminID`) VALUES ('$all_date', '$form_name', '$form_description', '$adminid') ";
 				$mysqli->query($q_INSERT_wfgeninfo);
-				
+/*
 				$q_SELECT_wfgeninfo = "SELECT * FROM `wfgeninfo` WHERE  `FormName` = '$form_name' ";
 				// get WfgenInfoID
 				$result_SELECT_wfgeninfo=$mysqli->query($q_SELECT_wfgeninfo);
-				while($row_SELECT_wfgeninfo=$result_SELECT_wfgeninfo->fetch_array()){ 
+				while($row_SELECT_wfgeninfo=$result_SELECT_wfgeninfo->fetch_array()){
 					$wfgeninfoID = $row_SELECT_wfgeninfo['WFGenInfoID'];
-				} 
-
+				}
 */
+
 
 
 
 }else{
 	echo "form name is not set";
-}*/
+}
 ?>
